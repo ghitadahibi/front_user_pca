ede87d3a612b5f733fb86b8fda2570905614909d
kenza dahibi <kenzadahibi@gmail.com>
2023-07-23 12:36:47 +0100

    thirdcommt

--- src/Sharedcomponent/container/Jobs.js
+++ src/Sharedcomponent/container/Jobs.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../container/Jobs.css';
-import bcp1 from "../../assets/bcp1_new.png"
-import bcp2 from "../../assets/BCP2.jpg"
-import bcp3 from "../../assets/bcp3.jpg"
+import bcp from "../../assets/bcp.jpg"
+import bcp12 from "../../assets/bcp12.jpeg"
+import bcp13 from "../../assets/bcp13.jpg"
 
 const Jobs = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -18,9 +18,9 @@ const Jobs = () => {
   return (
     <div>
       <div className="image-slider">
-        <img src={bcp1} alt="Image 1" className={currentImage === 0 ? "active" : ""} />
-        <img src={bcp2} alt="Image 2" className={currentImage === 1 ? "active" : ""} />
-        <img src={bcp3} alt="Image 3" className={currentImage === 2 ? "active" : ""} />
+        <img src={bcp} alt="Image 1" className={currentImage === 0 ? "active" : ""} />
+        <img src={bcp12} alt="Image 2" className={currentImage === 1 ? "active" : ""} />
+        <img src={bcp13} alt="Image 3" className={currentImage === 2 ? "active" : ""} />
       </div>
     </div>
   );
