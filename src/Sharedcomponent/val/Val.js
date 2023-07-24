3e8f84ed3b08824fb212ab8aeaa7afe1d61a5df0
kenza dahibi <kenzadahibi@gmail.com>
2023-07-24 10:26:03 +0100

    commit

--- src/Sharedcomponent/val/Val.js
+++ src/Sharedcomponent/val/Val.js
@@ -6,8 +6,9 @@ import Image2 from '../../assets/val.png'
 const Val = () => {
   return (
     <div className="container">
+       <p className="p">Pourquoi Nous Choisir </p>
       <div className="image-container">
-        <p className="p">Pourquoi Nous Choisir </p>
+       
         <img src={Image} alt="Image 1" />
         <img src={Image2} alt="Image 2" />
       </div>
