1237bdef9efa68e930a23259fe02a6964e1bc37e
dahibi ghita <dahibighita@gmail.com>
2023-08-01 14:34:10 +0200

    comit

--- src/Sharedcomponent/cards/Cards.js
+++ src/Sharedcomponent/cards/Cards.js
@@ -1,51 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Cards.css';
 
 function Carousel() {
   const [activeCardGroup, setActiveCardGroup] = useState(0);
-  const cardGroups = [
-    [
-      {
-        jobTitle: 'Développeur Web Full Stack',
-        jobDescription:
-          'Nous recherchons un développeur Web Full Stack pour rejoindre notre équipe et travailler sur des projets passionnants.',
-      },
-      {
-        jobTitle: 'Designer UX/UI',
-        jobDescription:
-          'Nous recherchons un designer UX/UI pour rejoindre notre équipe et travailler sur la conception d\'interfaces utilisateur intuitives et élégantes.',
-      },
-      {
-        jobTitle: 'Spécialiste Marketing Digital',
-        jobDescription:
-          'Nous recherchons un spécialiste du marketing numérique pour rejoindre notre équipe et mettre en œuvre des campagnes de marketing créatives et efficaces.',
-      },
-    ],
-    [
-      {
-        jobTitle: 'Développeur Mobile iOS',
-        jobDescription:
-          'Nous recherchons un développeur Mobile iOS pour rejoindre notre équipe et travailler sur des applications mobiles pour iOS.',
-      },
-      {
-        jobTitle: 'Développeur Front-End',
-        jobDescription:
-          'Nous recherchons un développeur Front-End pour rejoindre notre équipe et travailler sur la création d\'interfaces utilisateur pour le Web.',
-      },
-      {
-        jobTitle: 'Analyste de Données',
-        jobDescription:
-          'Nous recherchons un analyste de données pour rejoindre notre équipe et aider à l\'analyse et l\'interprétation de données importantes pour l\'entreprise.',
-      },
-    ],
-  ];
+  const [cardGroups, setCardGroups] = useState([]);
+  const [expandedCards, setExpandedCards] = useState(new Set());
+  const [viewedJobOffer, setViewedJobOffer] = useState(null);
 
-  const handlePrevClick = () => {
-    setActiveCardGroup((activeCardGroup - 1 + cardGroups.length) % cardGroups.length);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:10081/api/example/joboffer');
+      const data = await response.json();
+      const transformedData = data.map((jobOffer) => ({
+        jobTitle: jobOffer.jobOfferName,
+        jobDescription: jobOffer.content,
+      }));
+      const cardGroups = [];
+      for (let i = 0; i < transformedData.length; i += 3) {
+        cardGroups.push(transformedData.slice(i, i + 3));
+      }
+      setCardGroups(cardGroups);
+    };
+    fetchData();
+  }, []);
+
+  const renderJobDescription = (card) => {
+    const maxLines = 4;
+    const isExpanded = expandedCards.has(card.jobTitle);
+    const descriptionLines = card.jobDescription.split('\n');
+    const content = isExpanded ? card.jobDescription : descriptionLines.slice(0, maxLines).join('\n');
+    return (
+      <div>
+        <p>{content}</p>
+        {descriptionLines.length > maxLines && (
+          <a className="voir" href="#" onClick={() => handleViewClick(card)}>
+            {isExpanded ? 'Voir moins' : 'Voir offre complet'}
+          </a>
+        )}
+      </div>
+    );
+  };
+  const handleViewClick = (card) => {
+    setViewedJobOffer(card);
   };
 
+  const handleExpandClick = (cardTitle) => {
+    setExpandedCards((prevExpandedCards) => {
+      const newExpandedCards = new Set(prevExpandedCards);
+      if (newExpandedCards.has(cardTitle)) {
+        newExpandedCards.delete(cardTitle);
+      } else {
+        newExpandedCards.add(cardTitle);
+      }
+      return newExpandedCards;
+    });
+  };
+
+  const handlePrevClick = () => {
+    setActiveCardGroup((prevActiveCardGroup) =>
+      prevActiveCardGroup === 0 ? cardGroups.length - 1 : prevActiveCardGroup - 1
+    );
+  };
+  
   const handleNextClick = () => {
-    setActiveCardGroup((activeCardGroup + 1) % cardGroups.length);
+    setActiveCardGroup((prevActiveCardGroup) =>
+      prevActiveCardGroup === cardGroups.length - 1 ? 0 : prevActiveCardGroup + 1
+    );
   };
 
   const [showModal, setShowModal] = useState(false);
@@ -56,32 +76,32 @@ function Carousel() {
 
   const handleModalSubmit = async (event) => {
     event.preventDefault();
-  
+
     // Récupérer les valeurs du formulaire
     const job_name = document.getElementById('job_name').value;
     const cv = document.getElementById('cv').files[0];
-  
+
     // Créer un objet FormData pour envoyer les données de formulaire et les fichiers
     const formData = new FormData();
     formData.append('job_name', job_name);
     formData.append('cv', cv);
-  
+
     try {
       const response = await fetch('http://localhost:10081/api/example/calculate-similarity', {
         method: 'POST',
         body: formData
       });
-  
+
       if (!response.ok) {
         throw new Error('Une erreur est survenue lors de l\'appel à l\'API REST');
       }
-  
+
       const responseBody = await response.text();
       console.log('Réponse de l\'API REST :', responseBody);
     } catch (error) {
       console.error(error);
     }
-  
+
     setShowModal(false);
   }
 
@@ -95,14 +115,17 @@ function Carousel() {
 
   return (
     <div className="carousel">
+      
       <div className="carousel-container">
-        {cardGroups[activeCardGroup].map((card, index) => (
-          <div key={index} className="carousel-card">
-            <h2>{card.jobTitle}</h2>
-            <p>{card.jobDescription}</p>
-            <button className="apply-btn" onClick={handleApplyClick}>Postuler maintenant</button>
-          </div>
-        ))}
+      {cardGroups[activeCardGroup]?.map((card, index) => (
+  <div key={index} className="carousel-card">
+    <h2>{card.jobTitle}</h2>
+    {renderJobDescription(card)}
+    <button className="apply-btn" onClick={handleApplyClick}>Postuler maintenant</button>
+  </div>
+))}
+
+
       </div>
       <div className="carousel-buttons">
         <button
@@ -122,10 +145,10 @@ function Carousel() {
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={handleCloseModal}>&times;</span>
-            
-            <form  onSubmit={handleModalSubmit}>
+
+            <form onSubmit={handleModalSubmit}>
               <label htmlFor="firstName">Prénom:</label>
-             <input type="text" id="firstName" name="firstName" required />
+              <input type="text" id="firstName" name="firstName" required />
               <label htmlFor="lastName">Nom:</label>
               <input type="text" id="lastName" name="lastName" required />
               <label htmlFor="email">E-mail:</label>
@@ -140,7 +163,15 @@ function Carousel() {
           </div>
         </div>
       )}
+       {viewedJobOffer && (
+      <div className="glass-container">
+        <h2>{viewedJobOffer.jobTitle}</h2>
+        <p>{viewedJobOffer.jobDescription}</p>
+        <button className="fermer" onClick={() => setViewedJobOffer(null)}>Fermer</button>
+      </div>
+    )}
     </div>
+    
   );
 }
 
